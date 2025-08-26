a7e0631863594367805a01e156d9c2866cfce529
Vadim Liventsev <dev@vadim.me>
2025-08-26 22:59:02 +0200

    ell -> keeptalking

--- config.py
+++ config.py
@@ -1,13 +1,3 @@
-import os
-from openai import OpenAI
-import ell
 import logging
 
 logging.basicConfig(level=logging.INFO)
-
-client = OpenAI(
-    base_url='https://openrouter.ai/api/v1',
-    api_key=os.getenv("OPENROUTER_API_KEY"),
-    max_retries=50
-)
-ell.config.default_client = client
