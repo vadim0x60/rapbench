3f30edb38268abc4387b4a56c0540b4306f1c6ee
Vadim Liventsev <dev@vadim.me>
2025-09-03 23:06:47 +0200

    final results script

---
+++ results.py
@@ -0,0 +1,38 @@
+import glob
+from pathlib import Path
+import os
+
+N_ROUNDS = int(os.getenv('N_ROUNDS'))
+
+tiers = ['SSS', 'SS', 'S', 'A', 'B', 'C', 'D', 'F']
+contestants = []
+
+print('## Battles')
+
+for round in reversed(range(N_ROUNDS)):
+    round_dir = Path(f'tournament/round{round}')
+    with open(round_dir / 'contestants.txt') as f:
+        contestants.append(f.read().splitlines())
+
+    battle_n = 0
+    while True:
+        try:
+            battle_path = round_dir / f'{battle_n}.txt'
+            verdict_path = round_dir / f'{battle_n}.yml'
+            with open(battle_path) as f:
+                battle_title = f.read().splitlines()[0][2:]
+                print(f'- {battle_title} [lyrics]({battle_path}), [verdicts]({verdict_path})')
+        except FileNotFoundError:
+            break
+        battle_n += 1
+
+print('## Results')
+
+better_contestants = set()
+
+for tier, contestants in zip(tiers, contestants):
+    contestants = set(contestants)
+    contestants -= better_contestants
+    better_contestants.update(contestants)
+    contestants = ', '.join(contestants)
+    print(f'**{tier}**: {contestants}')
